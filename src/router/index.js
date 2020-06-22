b0ae9c5e0e6808b089d91028cfb7cef65e35dd0b
bystritskiy123 <gor_ell@mail.ru>
2020-06-22 21:19:04 +0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,7 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
+const routes = [{
     path: '/',
     name: 'Home',
     component: Home
@@ -16,7 +15,15 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/new',
+    name: 'new',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import( /* webpackChunkName: "about" */ '../views/new.vue')
   }
 ]
 
---
+++ src/views/new.vue
@@ -0,0 +1,20 @@
+<template>
+    <din id="main">
+        <h1>{{message}}</h1>
+    </din>
+</template>
+
+<script>
+    export default {
+        name: 'new',
+        data() {
+            return {
+                message: 'hello'
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
